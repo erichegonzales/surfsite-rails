c679f0eca21dcf56441c2230810bcd5b6b349cd5
Eriche Gonzales <erichelouise@gmail.com>
2022-10-05 16:49:23 -0400

    fixed infinite scroll bug in profile tabs

--- client/src/components/BookedLessonTab.js
+++ client/src/components/BookedLessonTab.js
@@ -15,6 +15,9 @@ const BookedLessonTab = () => {
     const getLessons = async () => {
       const res = await fetch(`http://localhost:3001/users/1/booked_lessons?page=1`);
       const data = await res.json();
+      if (data.length === 3) {
+        setHasMore(false);
+      }
       setLessons(data);
     };
 
