6a2050d53c487bd13111010408301d153f4830e2
Eriche Gonzales <erichelouise@gmail.com>
2022-10-06 02:14:07 -0400

    landing styles completed

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -12,28 +12,30 @@ const Landing = () => {
           <h3 className="blurb">A community where all surfers can connect</h3>
         </div>
         <div className="landing-login">
-          <h3>Login</h3>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+          <div className="login-box">
+            <h3>Login</h3>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </div>
         </div>
-        <div className="landing-create">
-          <h4>or</h4>
+        <span className="landing-create">
+          <h4 className="or">· or ·  </h4>
           <Button variant="primary" type="submit">
             Create an account
           </Button>
-        </div>
+        </span>
       </div>
       <div className="landing-bg"></div>
 
--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,11 +1,18 @@
 import { Container } from 'react-bootstrap'
-import { GiSurfBoard, GiWaveSurfer } from "react-icons/gi";
+import {
+  GiSurfBoard,
+  GiWaveSurfer,
+  GiWaveCrest,
+  GiFireWave,
+} from "react-icons/gi";
 
 const Logo = () => {
   return (
     <>
       <div className="logo">
         {/* <GiSurfBoard className="logo-icon" /> */}
+        {/* <GiFireWave className="logo-icon" /> */}
+        {/* <GiWaveCrest className="logo-icon" /> */}
         <GiWaveSurfer className="logo-icon" />
         <h5 className="logo-name">AllThingSurf</h5>
       </div>
